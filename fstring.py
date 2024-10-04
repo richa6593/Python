7a184c024f728fab31ad68da45dbaab20a3e62e8
Aakanchha Sharma <42612860+richa6593@users.noreply.github.com>
2024-10-04 08:54:31 +0530

    Create fstring.py

---
+++ fstring.py
@@ -0,0 +1,12 @@
+
+name= "Aakanchha"
+country="India"
+print(f"My name is {name} and I am from {country}")
+price=23.6789
+txt=f"I have {price:.2f} dollors"
+print(txt)
+print(f"{20*3}")
+print(type(f"{20*3}"))
+
+#fstring is valid from python 3.6 version onwards.
+# if u want to print fstring as it is without replacing its variable's value then use double curly brackets.
